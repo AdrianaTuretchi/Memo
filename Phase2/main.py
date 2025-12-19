c23553bba4d94b12e26732d1e7f50de9d52fec22
AdrianaTuretchi <161611434+AdrianaTuretchi@users.noreply.github.com>
2025-12-19 17:33:18 +0200

    Clean up main.py by removing unnecessary code
    Removed unused database operations and imports.

--- Phase2/main.py
+++ Phase2/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 import tkinter as tk
 from UI import NotesApp
@@ -14,21 +13,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-import sqlite3
-from CRUD import get_all_notes, update_note_title_content, delete_note_by_id
-
-try:
-    with sqlite3.connect('notes.db') as conn:
-        cursor = conn.cursor()
-        update_note_title_content(conn, "1","Nou","content nou")
-        delete_note_by_id(conn, "1")
-        print(get_all_notes(conn))
-except sqlite3.Error as e:
-    print(f"O eroare DB a oprit tranzacția: {e}")
 
 
 
-
-
->>>>>>> 0ed25b50bdc9f3d6eb1c92c243dd046c55582d16
